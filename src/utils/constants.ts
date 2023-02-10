a3afb52c4a8ef66c5ef4efb1057838c5635fa8ee
RajatSablok <rajat.main06@gmail.com>
2023-02-10 13:40:53 +0530

    fix: constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
-export const SEARCH_QUERY = 'backend development tutorial';
+export const SEARCH_QUERY = 'football';
 
 export const YOUTUBE_SEARCH_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-export const YOUTUBE_API_RATE_LIMIT = 10000;
+export const YOUTUBE_API_RATE_LIMIT = 100;
 
-export const YOUTUBE_API_HIT_FREQUENCY_IN_SECONDS = 10;
+export const YOUTUBE_API_HIT_FREQUENCY_IN_SECONDS = 20;
 
 export const FETCH_YOUTUBE_DATA_CRON_CONFIG = `*/${YOUTUBE_API_HIT_FREQUENCY_IN_SECONDS} * * * * *`;
